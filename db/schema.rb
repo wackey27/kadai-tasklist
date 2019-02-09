b7205c99b6a2b9bf6af4ab4914b90832e8f579fb
wataru.matsuoka <mwackey57@gmail.com>
2019-02-09 12:22:54 +0000

    task-list

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20190205061751) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "title"
+    t.string   "status"
   end
 
 end
